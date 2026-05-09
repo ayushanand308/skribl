79e43705c54b92638dd30e8336c58c56c7044a5c
ayushanand308 <ayushanand_mc21a11_34@dtu.ac.in>
2026-05-09 21:27:46 +0530

    fix : added transition for all combinations (9 events * 5 states = 45), changed transition table type from 'partial<record<k,v>>' to 'record<k,v>' to force all events to be as keys

--- server/src/game/gameStateMachine.ts
+++ server/src/game/gameStateMachine.ts
@@ -17,42 +17,76 @@ type events = "PICK_TIMER_EXPIRED"
 // a[state1][event1] = state2
 // a[state1][event2] = state3
 
-const transitions : Record<gameState, Partial<Record<events, gameState>>> = {
-    LOBBY :{
-        GAME_START : "PICK_WORD",
-        ALL_PLAYERS_LEFT : "GAME_END",
-        RESTART : "LOBBY"
-    },
-    PICK_WORD :{
-        PICK_TIMER_EXPIRED : "DRAW",
-        WORD_PICKED : "DRAW",
-        ALL_PLAYERS_LEFT : "GAME_END",
-        RESTART : "LOBBY"
-    },
-    DRAW : {
-        GUESS_TIMER_EXPIRED : "TURN_END",
-        ALL_GUESSED : "TURN_END",
-        ALL_PLAYERS_LEFT : "GAME_END",
-        RESTART : "LOBBY",
-        NEXT_TURN: "PICK_WORD",
-        ALL_ROUNDS_END: "GAME_END"
-    },
-    TURN_END : {
-        NEXT_TURN : "PICK_WORD",
-        ALL_ROUNDS_END : "GAME_END",
-        ALL_PLAYERS_LEFT : "GAME_END",
-        RESTART : "LOBBY"
-    },
-    GAME_END : {
-        RESTART : "LOBBY"
-    }
-}
+type StateNode = Record<events, gameState | "IGNORE">;
 
-function dispatch(currentState : gameState , event : events) : gameState {
+const transitions: Record<gameState, Record<events,gameState | 'IGNORE'>> = {
+    LOBBY: {
+        GAME_START: "PICK_WORD",
+        ALL_PLAYERS_LEFT: "GAME_END",
+        RESTART: "LOBBY",
+        PICK_TIMER_EXPIRED: "IGNORE",
+        WORD_PICKED: "IGNORE",
+        ALL_GUESSED: "IGNORE",
+        GUESS_TIMER_EXPIRED: "IGNORE",
+        NEXT_TURN: "IGNORE",
+        ALL_ROUNDS_END: "IGNORE"
+    },
+    PICK_WORD: {
+        PICK_TIMER_EXPIRED: "DRAW",
+        WORD_PICKED: "DRAW",
+        ALL_PLAYERS_LEFT: "GAME_END",
+        RESTART: "LOBBY",
+        GAME_START: "IGNORE",
+        ALL_GUESSED: "IGNORE",
+        GUESS_TIMER_EXPIRED: "IGNORE",
+        NEXT_TURN: "IGNORE",
+        ALL_ROUNDS_END: "IGNORE"
+    },
+    DRAW: {
+        GUESS_TIMER_EXPIRED: "TURN_END",
+        ALL_GUESSED: "TURN_END",
+        ALL_PLAYERS_LEFT: "GAME_END",
+        RESTART: "LOBBY",
+        NEXT_TURN: "PICK_WORD",
+        ALL_ROUNDS_END: "GAME_END",
+        GAME_START: "IGNORE",
+        PICK_TIMER_EXPIRED: "IGNORE",
+        WORD_PICKED: "IGNORE"
+    },
+    TURN_END: {
+        NEXT_TURN: "PICK_WORD",
+        ALL_ROUNDS_END: "GAME_END",
+        ALL_PLAYERS_LEFT: "GAME_END",
+        RESTART: "LOBBY",
+        GAME_START: "IGNORE",
+        PICK_TIMER_EXPIRED: "IGNORE",
+        WORD_PICKED: "IGNORE",
+        ALL_GUESSED: "IGNORE",
+        GUESS_TIMER_EXPIRED: "IGNORE"
+    },
+    GAME_END: {
+        RESTART: "LOBBY",
+        GAME_START: "IGNORE",
+        ALL_PLAYERS_LEFT: "IGNORE",
+        PICK_TIMER_EXPIRED: "IGNORE",
+        WORD_PICKED: "IGNORE",
+        ALL_GUESSED: "IGNORE",
+        GUESS_TIMER_EXPIRED: "IGNORE",
+        NEXT_TURN: "IGNORE",
+        ALL_ROUNDS_END: "IGNORE"
+    }
+};
+
+function dispatch(currentState: gameState, event: events): gameState {
     const nextState = transitions[currentState][event];
     
-    if(!nextState){ 
-        console.error(`[StateMachine] INVALID transition: ${currentState} + ${event}`);
+    if (nextState === "IGNORE") {
+        console.warn(`[IGNORE][StateMachine] : ${event} + ${currentState}`);
+        return currentState;
+    }
+
+    if (!nextState) { 
+        console.error(`[FATAL][StateMachine] : Fatal transition ${currentState} + ${event}`);
         throw new Error(`Not a valid transition for ${currentState} and ${event}`);
     }
 
